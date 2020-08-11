5ad9e2daee371cf4342ea54f60e808f48fd43ca6
Anastasiya4 <1914.smile@gmail.com>
2020-08-11 15:27:05 +0300

    itsok

--- task-05/ex5.js
+++ task-05/ex5.js
@@ -2,6 +2,6 @@
 const inputRef = document.querySelector('#name-input');
 const nameLabelRef = document.querySelector('#name-output');
 
-inputRef.addEventListener('change', event => {
+inputRef.addEventListener('input', event => {
     nameLabelRef.textContent = event.target.value || 'незнакомец';
 })
--- task-06/ex6.js
+++ task-06/ex6.js
@@ -2,8 +2,13 @@
  const inputRef = document.querySelector('#validation-input');
 
  function borderColor(event) {
- return event.target.value.length == inputRef.getAttribute('data-length') ? inputRef.setAttribute('class', 'valid')
- : inputRef.setAttribute('class', 'invalid');
- }
+// return event.target.value.length 
+if(Number(inputRef.getAttribute('data-length')) === event.target.value.length) {
+    inputRef.setAttribute('class', 'valid')}
+ else {
+     inputRef.setAttribute('class', 'invalid')}
+
+     return;
+ };
 
  inputRef.addEventListener('blur', borderColor);
